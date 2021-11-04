1414af0ae5249886f5752c384ed45e55edfca8e6
unknown <terencetan@hotmail.ca>
2021-11-04 13:11:06 -0400

    added modal for block 2

--- js/init.js
+++ js/init.js
@@ -1,1 +1,12 @@
-console.log("hello");
+document.addEventListener("click", function (e) {
+  console.log(e.target);
+  if (e.target.classList.contains("modal-item")) {
+    const src =
+      "https://live.staticflickr.com/65535/51655240554_af3631578e_b.jpg";
+    document.querySelector(".modal-img").src = src;
+    const myModal = new bootstrap.Modal(
+      document.getElementById("sports-modal")
+    );
+    myModal.show();
+  }
+});
